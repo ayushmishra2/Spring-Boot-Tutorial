76e1f5913571df320dd0cea54a0b914667653eea
Ayush Mishra <ayushlkw@gmail.com>
2026-02-11 17:48:48 +0530

    change api path to boomark controller

--- bookmark/src/main/java/com/K8s/bookmark/controller/BookmarkController.java
+++ bookmark/src/main/java/com/K8s/bookmark/controller/BookmarkController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/bookmark")
+@RequestMapping("/api/bookmarks")
 @RequiredArgsConstructor
 public class BookmarkController {
 
