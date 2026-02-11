abc7a287cbff636164d2e2ba8018944ea1f79362
Ayush Mishra <ayushlkw@gmail.com>
2026-02-11 12:32:58 +0530

    Committing the demo application for first time

---
+++ dockerTutorial/src/main/java/com/tutorial/docker/dockerTutorial/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.tutorial.docker.dockerTutorial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping
+    public String getMessage() {
+        return "Hello, Docker!";
+    }
+}
---
+++ dockerTutorial/src/test/java/com/tutorial/docker/dockerTutorial/DockerTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.docker.dockerTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
